fdd9d7b1c32cf5a765146aaf3b537d4432724873
rupertpumpkin <seansweeney7@gmail.com>
2014-04-09 10:22:03 -0400

    some bullshit

--- script.js
+++ script.js
@@ -1,80 +1,62 @@
-window.onload = function(){
- //===================================
-   //data
-   var rawCsvData = "";
-   var ajax = new XMLHttpRequest();
-   var recordsArray = [];
- //===================================
-   //event handlers
-   //get csv and populate email list
-   ajax.onload = function(){
-     if( ajax.status === 200 || ajax.status === 0){
-       rawCsvData = ajax.responseText;//capture raw csv and..
-       //split it into records array and ...
-       recordsArray = rawCsvData.split("\r");
-       populateDropdownList(recordsArray)
-     }
-     else{
-       alert("You've got ajax problems or remote file problems");
-     }
-   }
-   
-   //select a friend to email
-   id('selEmail').onchange = function(){
-     var i = id('selEmail').selectedIndex;
-     var chosenPerson = id('selEmail').options[i].text;
-     
-     //here's where we "fix" the name
-     //===============================
-     var pieces = chosenPerson.split(',');
-     chosenPerson = pieces[1] + " " + pieces[0];      
-     //===============================
-     var yes = confirm('Email '+ chosenPerson +'?');   
-     if(yes){
-         email(chosenPerson + " " + pieces[2]);
-     }
-     id('selEmail').selectedIndex = 0;// go back to email heading
-   };
+/*
+//========= "better???"  select alternitive with Jquery=====
+$(document).bind('mobileinit',function(){
+ $.mobile.selectmenu.prototype.options.nativeMenu = false;
+});
+//===================================
+*/
+//data
+var select = document.getElementById('select');
+var arrayOfPhoneNumbers = [];
+var ajax = new XMLHttpRequest();
+var record = [];
 
-   //===================================  
-   //functions
-   function id(identifier){
-     return document.getElementById(identifier);
-   }
-   
-   function email(name){
-     //alert('Emailing this person: ' + name);
-     
-     //Here's where we actually send the email
-     //=======================================
-     window.location.assign("mailto:"+name); 
-     //========================================
-   }
-   
-   //populate drop down list
-   function populateDropdownList(lines){        
-       id('selEmail').innerHTML = "";
-       var heading = document.createTextNode("Choose Friend Below");
-       var opt0 = document.createElement('option');
-       opt0.appendChild(heading);
-       id('selEmail').appendChild(opt0);
-       
-       //eliminate the csv header and then sort the names
-       for(var i = 1; i < lines.length ; i++){
-           lines[i-1] = lines[i];
+// ==========event handlers===========
+ajax.onload = function(){
+   if(ajax.status === 200 || ajax.status === 0){
+       arrayOfPhoneNumbers = ajax.responseText.split("\r");
+       for (var i = 1; i<arrayOfPhoneNumbers.length; i++){
+         arrayOfPhoneNumbers[i-1] = arrayOfPhoneNumbers[i];    
        }
-       lines.pop(); //pull out redundant line from the bottom of the list
-       lines.sort();
-       
-       //populate dropdown list
-       for(var i = 0; i < lines.length; i++){
-           var opt = document.createElement('option');
-           opt.appendChild(document.createTextNode(lines[i]));
-           id('selEmail').appendChild(opt);
+       arrayOfPhoneNumbers.pop();    
+       arrayOfPhoneNumbers.sort();
+
+       select.innerHTML = "";
+       for(var i = 0; i< arrayOfPhoneNumbers.length; i++){//fill the select object with phone number options
+         record = arrayOfPhoneNumbers[i].split(",");//get a record and slpit it up      
+         var name = record[0] + " " + record[1];//join first name and last name      
+         var text = document.createTextNode(name);//create text node for the option element      
+         var opt = document.createElement('option');//create option element      
+         opt.appendChild(text);//place text inside option element      
+         opt.value = record[2];//put in the phone number as the option's 'value'      
+         select.appendChild(opt);//append option to select element
        }
    }
-   //====================================     
-   //actions    
-   ajax.open('GET','friendMail.csv', true);
-   ajax.send(null);
 }
+
+select.onchange = function(){  
+   var i = select.selectedIndex;
+   location.assign("tel:" + select.options[i].value) ;
+}
+
+select.ondblclick = function(){
+   if(select.selectedIndex === 0){
+       var i = select.selectedIndex;
+       location.assign("tel:" + select.options[i].value) ;
+       
+       alert("slected index is 0");
+       return;
+   }
+   var i = select.selectedIndex;
+   location.assign("tel:" + select.options[i].value) ;
+}
+
+select.onclick = function(){
+   var i = select.selectedIndex;
+   location.assign("tel:" + select.options[i].value) ;
+}
+
+//activities
+var doc = "http://dl.dropboxusercontent.com/u/21142484/.Mobile.Apps/.Final.Weeks/Dialer/PhoneNumbers.csv"
+ajax.open("GET",doc,true);
+ajax.send();
